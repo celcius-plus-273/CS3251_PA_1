4fcba0e49bbc2904fc4825ecd970d06cdcbb286a
celcius-plus-273 <kelvinandreshau@gmail.com>
2023-02-18 19:36:24 -0500

    credentials, chatlog, and multiple user support

--- client.py
+++ client.py
@@ -6,44 +6,77 @@ import sys
 #TODO: Implement a client that connects to your server to chat with other clients here
 
 # Use sys.stdout.flush() after print statemtents
+msg = ""
 
-def msg_input(clientSocket):
+def msg_input(clientSocket, last_msg):
 	while True:
-		msg = clientSocket.recv(1024).decode()
-		print(f"From server: {msg}")
-		if msg == "Stop":
+		print(last_msg)
+		if last_msg == ":Exit":
 			break
+		new_msg = clientSocket.recv(1024).decode()
+		print(f"{new_msg}")
 
 def msg_output(clientSocket):
 	while True:
+		global msg
 		msg = input("")
 		clientSocket.send(msg.encode())
-		
-		if msg == "Stop":
+		if msg == ":Exit":
 			break
-	
 
 if __name__ == "__main__":
+	# read and parse command line arguments
+	if (len(sys.argv) != 10):
+		print("Cannot start client with given parameters")
+		sys.exit()
+	else:
+		if (sys.argv[1] != "-join"):
+			print("First argument should be \"join\"!")
+			sys.exit()
+		elif (sys.argv[2] != "-host"):
+			print("Second argumet should be \"host\"!")
+		elif (sys.argv[4] != "-port"):
+			print("Fourth argumet should be \"port\"!")
+			sys.exit()
+		elif (sys.argv[6] != "-username"):
+			print("Sixth argumet should be \"username\"!")
+			sys.exit()
+		elif (sys.argv[8] != "-passcode"):
+			print("Eight argumet should be \"passcode\"!")
+			sys.exit()
+
+		HOST = sys.argv[3]
+		PORT = int(sys.argv[5])
+		USERNAME = sys.argv[7]
+		PASSWORD = sys.argv[9]
+
+	# initialize client TCP socket
 	try:
 		clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		print("Client socket created succesfully")
+		#print("Client socket created succesfully")
 	except socket.error as err:
 		print("Client socket creation failed. Error: %s" %(err))
 
+	# attemp connection to server
+	try:
+		clientSocket.connect(("127.0.0.1", PORT))
+	except socket.error as err:
+		print(f"Couldn't establich connection with host: {HOST}")
+	
+	# send credentials for login
+	clientSocket.send(PASSWORD.encode())
+	if (clientSocket.recv(1024).decode() == "Allowed"):
+		print(f"Connected to {HOST} on port {PORT}")
+		clientSocket.send(USERNAME.encode())
+	else:
+		print("Invalid credentials. Try again.")
+		clientSocket.close()
+		sys.exit()
 
-	#ip = socket.gethostbyname(socket.gethostname())
-	HOST = "127.0.0.1"
-	PORT = 1309
-
-	clientSocket.connect(("127.0.0.1", PORT))
-	print(f"Succesfully connected to server with address: {HOST}")
-
-	msg = clientSocket.recv(1024).decode()
-	print(f"Message from server: {msg}")
 
 	#set up two threads: 1) receives messages 2) sends messages
 	output_thread = threading.Thread(target=msg_output, args=(clientSocket,))
-	input_thread = threading.Thread(target=msg_input, args=(clientSocket,))
+	input_thread = threading.Thread(target=msg_input, args=(clientSocket, msg, ))
 	
 	output_thread.start()
 	input_thread.start()
--- server.py
+++ server.py
@@ -1,62 +1,142 @@
 import socket
 import threading
 import sys
-import time
 
 
 #TODO: Implement all code for your server here
 
 # Use sys.stdout.flush() after print statemtents
 
-def repeat_msg(client):
+def find_username(msg):
+	n = 0
+	while ( n < len(msg) and (msg[n] != ":")):
+		n += 1
+	return msg[0 : n]
+
+def chatroom_conection_input(all_clients, client, buffer, chat_lock, client_lock):
 	while True:
-		client.send("Are you receving?".encode())
-		time.sleep(7)
+		msg = client.recv(1024).decode()
+		username = all_clients[client]
+		
+		match msg:
+			case ":Exit":
+				# delete cient from client dictionary
+				client_lock.acquire()
+				all_clients.pop(client)
+				client_lock.release()
+
+				# send a message informming others that client has left
+				chat_lock.acquire()
+				buffer.append(f"{username} left the chatroom")
+				chat_lock.release()
+
+				# close connection with client
+				client.close()
+				break
+
+			case _:
+				chat_lock.acquire()
+				buffer.append(f"{username}: {msg}")
+				chat_lock.release()
+				
+def chatroom_output(all_clients, buffer, lock):
+	while True:
+		while len(buffer) != 0:
+			lock.acquire()
+			msg = buffer[0]
+			msg_username = find_username(msg)
+			print(msg)
+			for client in all_clients:
+				if (all_clients[client] == msg_username):
+					continue
+				client.send(msg.encode())
+			buffer.pop(0)
+			lock.release()
+
 
 if __name__ == "__main__":
+	# read and parse command line arguments
+	if (len(sys.argv) != 6):
+		print("Can't initialize server with given parameters")
+		sys.exit()
+	else:
+		if (sys.argv[1] != "-start"):
+			print("First argument should be \"start\"!")
+			sys.exit()
+		elif (sys.argv[2] != "-port"):
+			print("Second argumet should be \"port\"!")
+			sys.exit()
+		elif (sys.argv[4] != "-passcode"):
+			print("Third argumet should be \"passcode\"!")
+			sys.exit()
+
+		PORT = int(sys.argv[3])
+		PASSWORD = sys.argv[5]
+
+	# initialize a server TCP socket
 	try:
 		serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		print("Server socket created succesfully")
-		sys.stdout.flush()
+		#print("Server socket created succesfully")
+		#sys.stdout.flush()
 	except socket.error as err:
 		print("Server socket creation failed. Error: %s" %(err))
 		sys.stdout.flush()
 
-	PORT = 1309
-
+	# bind the server socket to port 127.0.0.1 (loopback) and given port from cml argument
 	try:
 		serverSocket.bind(("127.0.0.1", PORT))
-		print("Server socket was succesfully binded to port {}".format(PORT))
-		sys.stdout.flush()
+		#print("Server socket was succesfully binded to port {}".format(PORT))
+		#sys.stdout.flush()
 	except socket.error as err:
 		print("Server socket binding to port {} failed".format(PORT))
 		sys.stdout.flush()
 
+	# initialize chatlog for chatroom
+	chat_lock = threading.Lock()
+	chat_buffer = []
+
+	# intialize client dictionary
+	clients_lock = threading.Lock()
+	all_clients = {}
+
+	# start listening for clients
 	serverSocket.listen(3)
 	print("Server is listening...")
 
-	num = 1
+	# initialize and start the chatroom output thread
+	server_output_thread = threading.Thread(target=chatroom_output, args=(all_clients, chat_buffer, chat_lock,))
+	server_output_thread.start()
 
+	num_clients = 0
 	while(True):
 		client, addr = serverSocket.accept()
+		num_clients += 1
+		if (client.recv(1024).decode() == PASSWORD):
+			# send authorization to client
+			client.send("Allowed".encode())
 
-		print(f"Client: {num} has succesfully connected")
+			# receive username from client
+			USERNAME = client.recv(1024).decode()
+
+			# send a welcome message to all other clients and server
+			print(f"{USERNAME} joined the chatroom")
+			for other_client in all_clients:
+				other_client.send(f"{USERNAME} joined the chatroom".encode())
+
+			# appends client to array of clients
+			clients_lock.acquire()
+			all_clients[client] = USERNAME
+			clients_lock.release()
+
+			client_thread = threading.Thread(target=chatroom_conection_input, args=(all_clients, client, chat_buffer, chat_lock, clients_lock, ))
+			client_thread.start()
+
+		else:
+			client.send("Not Allowed".encode())
+			print("Client not allowed")
+			client.close()
+
 		
-		client.send(f"You're client {num}".encode())
-		#num += 1
-		repeat_thread = threading.Thread(target=repeat_msg, args=(client,))
-		repeat_thread.start()
 
 
-		while True:
-			msg = client.recv(1024).decode()
-			client.send(msg.encode())
-			print(f"Echoed message: {msg}")
-			if msg == "Stop":
-				break
-		
-		
-		repeat_thread.join()
-		client.close()
-		print(f"Client {num} has been disconnected")
-		num += 1
+
